8f7ea64b1a0355fd66d39cde91c741070ce7b320
Alexis Tremblay <alexis.tremblay@gmail.com>
2023-09-10 11:22:36 -0400

    First commit;

--- main.ts
+++ main.ts
@@ -1,28 +1,116 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, addIcon, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface ArXivPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+interface PaperData {
+  title: string;
+  authors: string;
+  publicationDate: string;
+  summary: string;
+  id: string;
+  url: string;
+  pdfLink: string;
+}
+
+const DEFAULT_SETTINGS: ArXivPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class ArxivPlugin extends Plugin {
+	//settings: ArXivPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+	async fetchPaper(paperId: string): Promise<void> {
+		if (paperId) {
+			try {
+				const data = await this.fetchPaperData(paperId);
+				await this.createNote(data);
+			} catch (error) {
+				console.error('Error fetching paper data:', error);
+				new Notice(`Error fetching paper data: ${paperId}`);
+			}
+		}
+	}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+	fetchPaperData(id: string): Promise<PaperData> {
+		return new Promise((resolve, reject) => {
+			const url = `http://export.arxiv.org/api/query?id_list=${id}`;
+				const xhr = new XMLHttpRequest();
+			xhr.open('GET', url);
+			xhr.onreadystatechange = () => {
+				if (xhr.readyState === 4) {
+					if (xhr.status === 200) {
+						const parser = new DOMParser();
+						const xmlDoc = parser.parseFromString(xhr.responseText, 'application/xml');
+						const entry = xmlDoc.querySelector('entry');
+
+						if (entry) {
+							const title = entry.querySelector('title')?.textContent || 'No Title';
+							const authors = Array.from(entry.querySelectorAll('author name')).map(el => el.textContent).join(', ') || 'No Authors';
+							const publicationDateRaw = entry.querySelector('published')?.textContent;
+							const publicationDate = publicationDateRaw ? (new Date(publicationDateRaw)).toISOString().split('T')[0] : 'No Publication Date';
+							const url = entry.querySelector('id')?.textContent || 'No URL';
+							const summary = entry.querySelector('summary')?.textContent || 'No Summary';
+							const pdfLink = xmlDoc.querySelector('link[title="pdf"]')?.getAttribute('href') || 'No PDF Link';
+
+							resolve({ title, authors, publicationDate, summary , id, url, pdfLink});
+						} else {
+							reject(new Error('No entry found in the response'));
+						}
+					} else {
+						reject(new Error('Failed to fetch data'));
+					}
+				}
+			};
+			xhr.send();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	}
+
+	async createNote(data: PaperData): Promise<void> {
+		const authorsList = data.authors.split(', ').map(author => `  - "${author}"`).join('\n');
+		const frontMatter = `---
+title: "${data.title}"
+authors:
+${authorsList}
+published: "${data.publicationDate}"
+arXiv: "${data.url}"
+paper: "${data.pdfLink}"
+---
+`;
+
+		const content = `
+#### Abstract
+
+${data.summary}
+		`;
+
+		await this.app.vault.create(`${data.id}.md`, frontMatter + content);
+		new Notice(`Created note: ${data.id}`);
+	}
+	async onload() {
+		//await this.loadSettings();
+
+		try {
+			const arXivLogo = await this.readSVGFile();
+			addIcon('arXiv', arXivLogo);
+			addIcon("circle", `<circle cx="50" cy="50" r="50" fill="currentColor" />`);
+
+			const ribbonIconEl = this.addRibbonIcon("arXiv", "arXiv fetch paper", () => {
+				new ArXivModal(this.app, (paperId) => {
+				this.fetchPaper(paperId);
+				}).open();
+			});
+			ribbonIconEl.addClass('my-plugin-ribbon-class');
+			//console.log('Received data:', data);
+		} catch (error) {
+			console.error('Error:', error);
+		}
+
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
@@ -30,52 +118,18 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-arXiv-modal',
+			name: 'Fetch arXiv paper info',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				new ArXivModal(this.app, (paperId) => {
+				this.fetchPaper(paperId);
+				}).open();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		//this.addSettingTab(new ArXivSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -89,28 +143,33 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+	async readSVGFile(): Promise<string> {
+
+		const filePath = path.resolve(
+			this.app.vault.adapter.basePath, 
+			this.manifest.dir,
+			'arxiv-logomark.svg'
+		);
+		console.log(filePath);
+
+		return new Promise((resolve, reject) => {
+			fs.readFile(filePath, 'utf8', (err, data) => {
+				if (err) {
+					console.error('Error reading SVG file:', err);
+					reject(err);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class ArXivSettingTab extends PluginSettingTab {
+	plugin: ArxivPlugin;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ArxivPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -132,3 +191,43 @@ class SampleSettingTab extends PluginSettingTab {
 				}));
 	}
 }
+
+class ArXivModal extends Modal {
+  result: string;
+  onSubmit: (result: string) => void;
+
+  constructor(app: App, onSubmit: (result: string) => void) {
+    super(app);
+    this.onSubmit = onSubmit;
+  }
+
+  onOpen() {
+    const { contentEl } = this;
+
+
+    new Setting(contentEl)
+      .setName("arXiv id:")
+      .addText((text) =>
+        text.onChange((value) => {
+          this.result = value
+        }));
+
+    new Setting(contentEl)
+      .addButton((btn) =>
+        btn
+          .setButtonText("Fetch")
+          .setCta()
+          .onClick(() => {
+            this.close();
+            this.onSubmit(this.result);
+          }));
+  }
+
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
+}
+
+export default ArxivPlugin;
+
