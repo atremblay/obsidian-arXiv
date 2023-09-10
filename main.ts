3c7ed9b7456684f8c1f84385cbde7bc96d4a1e91
Alexis Tremblay <alexis.tremblay@gmail.com>
2023-09-10 11:32:57 -0400

    remove debug log

--- main.ts
+++ main.ts
@@ -150,7 +150,6 @@ ${data.summary}
 			this.manifest.dir,
 			'arxiv-logomark.svg'
 		);
-		console.log(filePath);
 
 		return new Promise((resolve, reject) => {
 			fs.readFile(filePath, 'utf8', (err, data) => {
